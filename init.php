2428e9d010ddd03ec9c8dbd81ae45f7c823d23fe
Eduardo Pacheco <eduardo.pacheco@huia.com.br>
2015-04-20 10:04:58 -0300

    - Incluir modulo de email no bootstrap.

--- init.php
+++ init.php
@@ -1,5 +1,12 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
+// autoload shadowhand-email
+Kohana::modules(
+	array_merge(Kohana::modules(), array(
+		'shadowhand-email' => VENDORPATH.'shadowhand/email'
+	))
+);
+
 Route::set('huia_email', 'huia/email(/<action>(/<id>))')
 	->defaults(array(
 		'controller' => 'email',
