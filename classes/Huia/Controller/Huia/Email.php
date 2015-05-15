0a96129d4f8a462f0bdd15440deabab6cacb8d01
Eduardo Pacheco <eduardo.pacheco@huia.com.br>
2015-05-15 13:32:32 -0300

    - Correção de indentação.

--- classes/Huia/Controller/Huia/Email.php
+++ classes/Huia/Controller/Huia/Email.php
@@ -1,22 +1,22 @@
 <?php defined('SYSPATH') OR die('No direct script access.');
 
 class Huia_Controller_Huia_Email extends Controller {
-	
-	public function action_index()
-	{
-		$config = Kohana::$config->load('huia/email');
-		
-		$values = Arr::map('strip_tags', $this->request->post());
+  
+  public function action_index()
+  {
+    $config = Kohana::$config->load('huia/email');
+    
+    $values = Arr::map('strip_tags', $this->request->post());
 
-		$view = View::factory('huia/email/'.$values['view']);
-		$view->set($values);
+    $view = View::factory('huia/email/'.$values['view']);
+    $view->set($values);
 
-		$result = Email::factory($values['subject'], $view->render(), 'text/html')
-										->to($values['to_email'])
-										->from($config->from_email, $config->from_name)
-										->send();
+    $result = Email::factory($values['subject'], $view->render(), 'text/html')
+                    ->to($values['to_email'])
+                    ->from($config->from_email, $config->from_name)
+                    ->send();
 
-		$this->response->body(@json_encode(array('success' => $result)));		
-	}
-	
+    $this->response->body(@json_encode(array('success' => $result)));   
+  }
+  
 }
--- init.php
+++ init.php
@@ -2,14 +2,14 @@
 
 // autoload shadowhand-email
 Kohana::modules(
-	array_merge(Kohana::modules(), array(
-		'shadowhand-email' => VENDORPATH.'shadowhand/email'
-	))
+  array_merge(Kohana::modules(), array(
+    'shadowhand-email' => VENDORPATH.'shadowhand/email'
+  ))
 );
 
 Route::set('huia_email', 'huia/email(/<action>(/<id>))')
-	->defaults(array(
-		'controller' => 'email',
-		'action'     => 'index',
-		'directory' => 'huia'
-	));
+  ->defaults(array(
+    'controller' => 'email',
+    'action'     => 'index',
+    'directory' => 'huia'
+  ));
